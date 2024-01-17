3346059a86755a90fa63bd67ecee71990c095a0e
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2024-01-17 20:44:21 +0100

    App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import fetchData from "@/app/utils/fetchData";
 import Link from "next/link";
 import PlanetTimeDetails from "@/layout/PlanetTimeDetails";
-import MainBar from "@/layout/MainBar/MainBar";
+import MainBar from "../layout/MainBar/MainBar";
 export default function Home() {
   const planetData = fetchData("mercury");
   return (
