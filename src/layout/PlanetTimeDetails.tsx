232810e102f79df93ab015979f80eb91f22976ed
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2024-01-19 20:09:23 +0100

    layout

--- src/layout/PlanetTimeDetails.tsx
+++ src/layout/PlanetTimeDetails.tsx
@@ -6,7 +6,7 @@ export default function PlanetTimeDetails({
   planetData: DataType | undefined;
 }) {
   return (
-    <div className="px-6 sm:px-0 space-y-2 sm:space-y-0 sm:flex sm:space-x-3 mb-20 lg:mb-0 sm:row-start-3 sm:col-span-2 lg:space-x-[1.875rem]">
+    <div className="px-6 sm:px-0 space-y-2 sm:space-y-0 sm:flex sm:space-x-3 mb-20 sm:row-start-3 sm:col-span-2 lg:space-x-[1.875rem]">
       <div className="uppercase flex sm:flex-col px-6 sm:px-4 items-center sm:items-start justify-between border-[1px] border-FFFFF border-opacity-10 sm:w-full">
         <p className="py-4 sm:pb-0 text-H4Tablet lg:text-H4Desktop opacity-50">
           Rotation Time
